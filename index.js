348ff8ae322161b32b2e89e0b9825a40700cc9ed
Jacob Moss <cob.mossy@gmail.com>
2017-03-09 17:07:19 +0000

    adds the extraction of names from the emails

--- index.js
+++ index.js
@@ -1,12 +1,75 @@
-var textract = require('textract');
+const textract = require('textract');
+const summarizer = require('nodejs-text-summarizer')
+const process = require("process");
 const cullKeywords = require('cull-keywords');
+const fs = require( 'fs' );
+const path = require( 'path' );
 
-textract.fromFileWithPath("./data/COMP103P-2016-Poster/1st_team03.pptx", { preserveLineBreaks: true }, function( error, text ) {
-  console.log(error);
-  console.log(text);
+var dirPath = "./data/COMP103P-2016-Poster/";
 
-  cullKeywords(text, (err, results) => {
-    console.log(results);
+function capitalise(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function extractName(string) {
+  string = string.substr(0, string.indexOf('@')); // remove @ucl.ac.uk ending
+  string = string.replace(/\.[0-9]+/g, '');       // remove any .15 after name
+  string = string.split(".").map(capitalise);     // split by dot and capitalise each
+  console.log(string);
+  return string.join(" ");
+}
+
+function extractNamesFromText(string) {
+    var emails = string.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+    if (emails == null) {
+      return [];
+    } else {
+      return emails.map(extractName);
+    }
+}
+
+fs.readdir(dirPath, function(err, files) {
+  if(err) {
+      console.error("Could not list the directory", err);
+      process.exit(1);
+  }
+
+  files.forEach(function(file, index) {
+    var extension = path.extname(file);
+    if (extension == ".info") {
+      return;
+    }
+    var filePath = path.join(dirPath, file);
+    var outputPath = path.join(dirPath, path.basename(file, extension) + ".info");
+    console.log(outputPath);
+    textract.fromFileWithPath(filePath, { preserveLineBreaks: true }, function(error, text) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+
+      //console.log(text);
+
+      cullKeywords(text, function(err, results) {
+        //console.log(results);
+        var names = JSON.stringify(extractNamesFromText(this.text));
+        console.log(names);
+        var keywords;
+        if (!results || !results.keywords || results.keywords.length <= 0) {
+          keywords = "";
+        } else {
+          keywords = JSON.stringify(results.keywords.map(s => s.toLowerCase()));
+        }
+        fs.writeFile(this.output, "{ text: \"" + this.text + "\", authors: " + names + " tags: " + keywords + "}", function(err) {
+            if(err) {
+                return console.log(err);
+            }
+
+        });
+      }.bind({ text: text, output: this.output }));
+
+      //console.log(summarizer(text));
+
+    }.bind({ output: outputPath }));
   });
-
 });
