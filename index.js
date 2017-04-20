43ed96d85db089d38ddf354105f45c95c257cf79
Jacob Moss <cob.mossy@gmail.com>
2017-04-20 18:40:55 +0100

    add more extraction functions

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ const TITLE_INDEX = 1;
 const CLIENT_INDEX = 3;
 const YOUTUBE_INDEX_103P = 4;
 const YOUTUBE_INDEX_GC02 = 6;
+const TEAM_LEADER_103P = 6;
+const TEAM_LEADER_GC02 = 7;
 
-var what = COMP103P;
 var gc02InfosPath = 'data/GC02-infos.csv';
 var comp103pInfosPath = 'data/COMP103P-infos.csv';
 
@@ -42,16 +43,44 @@ for (var line of data) {
 }
 
 var dirPath = "./data/COMP103P-2016-Poster/";
-f(dirPath);
-dirPath = "./data/COMP103P-2016-Poster/";
-f(dirPath);
+f(dirPath, COMP103P);
+
+dirPath = "./data/COMPGC02-2015-Poster/";
+f(dirPath, GC02);
 
 function capitalise(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function extractTeamNumber(string) {
-  if (what == GC02) {
+function getModule(m) {
+  if (m == GC02) {
+    return "COMPGC02";
+  } else {
+    return "COMP103P";
+  }
+}
+function extractTeam(id, m) {
+  var team = [];
+  for (var i = 0; i < 3; i++) {
+    if (m == GC02) {
+      if (gc02Objects[id][TEAM_LEADER_GC02 + i] != undefined) {
+        team.push(gc02Objects[id][TEAM_LEADER_GC02 + i]);
+      }
+    } else {
+      if (comp103pObjects[id] == undefined) {break;}
+      if (!isNullOrEmpty(comp103pObjects[id][TEAM_LEADER_103P + i])) {
+        team.push(comp103pObjects[id][TEAM_LEADER_103P + i]);
+      }
+    }
+  }
+  return team;
+}
+function isNullOrEmpty(string) {
+  string = string.trim().replace(/(\r\n|\n|\r)/gm,"");  
+  return !string;
+}
+function extractTeamNumber(string, m) {
+  if (m == GC02) {
     return parseInt(string);
   } else {
     return parseInt(string.substr(-2));
@@ -67,19 +96,19 @@ function extractKeywords(text) {
   return keywords;
 }
 
-function extractTitle(id) {
-  if (what == GC02) {
-    return gc02Objects[id][TITLE_INDEX];
+function extractTitle(id, m) {
+  if (m == GC02) {
+    return gc02Objects[id][TITLE_INDEX].replace(/[\u2018\u2019]/g, "'");
   } else {
     if (comp103pObjects[id] == undefined) {
       return `Team ${id}`;
     }
-    return comp103pObjects[id][TITLE_INDEX];
+    return comp103pObjects[id][TITLE_INDEX].replace(/[\u2018\u2019]/g, "'");
   }
 }
 
-function extractClient(id) {
-  if (what == GC02) {
+function extractClient(id, m) {
+  if (m == GC02) {
     return gc02Objects[id][CLIENT_INDEX];
   } else {
     if (comp103pObjects[id] == undefined) {
@@ -89,8 +118,8 @@ function extractClient(id) {
   }
 }
 
-function extractYoutubeLink(id) {
-  if (what == GC02) {
+function extractYoutubeLink(id, m) {
+  if (m == GC02) {
     return gc02Objects[id][YOUTUBE_INDEX_GC02];
   } else {
     if (comp103pObjects[id] == undefined) {
@@ -100,17 +129,25 @@ function extractYoutubeLink(id) {
   }
 }
 
+function getModuleDate(m) {
+  if (m == GC02) {
+    return "2015-06-01";
+  } else {
+    return "2016-06-01";
+  }
+}
+
 function convertToImage(file) {
   var extension = path.extname(file);
-    if (extension == ".pdf") {
-      var pdfImage = new PDFImage(file);
-      pdfImage.convertPage(0).then(function (imagePath) {});
-    } else {
-      ppt2png(file, "./" + path.dirname(file) + "/" + path.basename(file, extension) + '-0', function(err) {
-        console.log("ppt2png error: ", err);
-      });
-    }
-    return path.basename(file, extension) + '-0.png';
+  if (extension == ".pdf") {
+    var pdfImage = new PDFImage(file);
+    pdfImage.convertPage(0).then(function (imagePath) {});
+  } else {
+    ppt2png(file, "./" + path.dirname(file) + "/" + path.basename(file, extension) + '-0', function(err) {
+      console.log("ppt2png error: ", err);
+    });
+  }
+  return path.basename(file, extension) + '-0.png';
 }
 
 function extractName(string) {
@@ -128,9 +165,8 @@ function extractNamesFromText(string) {
       return emails.map(extractName);
     }
 }
-const moduleClass = "COMP103P";
 
-function f(dirPath) {
+function f(dirPath, what) {
   fs.readdir(dirPath, function(err, files) {
     if(err) {
         console.error("Could not list the directory", err);
@@ -151,35 +187,41 @@ function f(dirPath) {
           console.log("errr: ", error);
           return;
         }
-
-        var names = JSON.stringify(extractNamesFromText(text));
         
         const keywords = extractKeywords(text);
-        const id = extractTeamNumber(this.name);
-        const title = extractTitle(id);
-        const client = extractClient(id);
+        const id = extractTeamNumber(this.name, this.thisModule);
+        const title = extractTitle(id, this.thisModule);
+        console.log(`title: ${title}`)
+        const client = extractClient(id, this.thisModule);
+        const names = JSON.stringify(extractTeam(id, this.thisModule));
         const imageUrl = convertToImage(this.file);
-        const youtube = extractYoutubeLink(id);
+        const youtube = extractYoutubeLink(id, this.thisModule);
+        const mdPath = path.dirname(this.output) + `/output/${getModuleDate(this.thisModule)}-${title}.md`;
+        const moduleName = getModule(this.thisModule);
 
-        fs.writeFile(this.output, `
-        ---
-        title: ${title}
-        authors: ${names}
-        tags: ${keywords}
-        client: ${client}
-        score: -1
-        image: "/images/${imageUrl}"
-        module: "${moduleClass}"
-        youtube: "${youtube}"
-        ---
-        ![]({{ site.baseurl }}/images/${imageUrl})`, function(err) {
+        if (!fs.existsSync(path.dirname(this.output) + `/output/`)){
+          fs.mkdirSync(path.dirname(this.output) + `/output/`);
+        }
+        fs.writeFile(mdPath, 
+`---
+title: "${title}"
+authors: ${names}
+tags: ${keywords}
+client: "${client}"
+score: -1
+image: "/images/${imageUrl}"
+module: "${moduleName}"
+youtube: "${youtube}"
+excerpt: ""
+---
+![]({{ site.baseurl }}/images/${imageUrl})`, function(err) {
             if(err) {
                 console.log("error: ", err);
             }
 
         });      
 
-      }.bind({ output: outputPath, name: savedName, file: filePath }));
-    });
-  });
+      }.bind({ output: outputPath, name: savedName, file: filePath, thisModule: this.what }));
+    }.bind({ what: this.what }));
+  }.bind({ what: what }));
 }
